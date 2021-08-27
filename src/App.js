faf80bb161d16f1e99abcfeff07f10d2d40d74ee
MohammadEsFa <msf3557417@gmail.com>
2021-08-27 12:22:04 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <nav className="navbar  navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">SHOPPING-CART</a>
+          <a className='admin'>Admin</a>
+        </nav>
+        <main>
+          <h1>products list</h1>
+        </main>
+        <footer>All Rights Reserved</footer>
+    </div>
+  );
+}
+
+export default App;
