a14df22deec1f1becb40700e601a6afa49a78c96
MohammadEsFa <msf3557417@gmail.com>
2021-08-27 22:15:34 -1000

    filter-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //feature 1
 import React , {Component} from 'react';
 
+import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json'
 
@@ -15,9 +16,31 @@ class App extends Component {
     }
   }
 
+  handleSort = (event) => {
+    const sort = event.target.value
+    const myproducts = [...data.products]
+    this.setState({
+      sort,
+      products : myproducts.sort((a,b)=>(
+        sort === 'Lowset' ?
+        ((a.price > b.price) ? 1:-1):
+        sort === 'Highest' ?
+        ((a.price < b.price) ? 1:-1):
+        ((a._id > b._id) ? 1:-1)
+      ))
+    })
+  }
+
+  
+  handleSize = (event) => {
+    event.target.value === "" ? 
+    this.setState({size:event.target.value , products : data.products}) 
+    :
+    this.setState({size:event.target.value , products : data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)})
+  }
+ 
+
   render() {
-
-
 
     return (
       <div className="App">
@@ -28,6 +51,13 @@ class App extends Component {
           <main>
             <div className='content'>
               <div className='main'>
+                <Filter 
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                handlesort={this.handleSort}
+                handlesize={this.handleSize}
+                />
                 <Products products={this.state.products}/>
               </div>
               <div className='sidebar'>Cart Items </div>
